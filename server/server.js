766424ad7cd39f9d050502c55cfc9780dde0a05b
Davidnino21 <davidnino_21@yahoo.com>
2024-02-27 21:30:31 -0800

    update

--- server/server.js
+++ server/server.js
@@ -22,11 +22,7 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-} 
+}
 
 app.use(routes);
 
@@ -37,6 +33,7 @@ const startApolloServer = async () => {
 
   db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    
   });
 }
 
